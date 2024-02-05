019470a845bb0dac29c46a54bee9c508fa1f9105
henricaodopao1 <dragonsxbr@gmail.com>
2024-02-05 12:53:35 +0000

    X00QD: build android.hardware.media.omx@1.0-impl

--- device.mk
+++ device.mk
@@ -215,6 +215,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
     android.hardware.media.omx@1.0-service \
     libstagefright_omx.vendor
     
