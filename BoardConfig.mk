d4c1f7e2dcaafaebca96bbf7f3dba6c6dde7e496
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    sdm660-common: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,9 @@ DEVICE_PATH := device/asus/X00QD
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# A/B
+AB_OTA_UPDATER := false
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
