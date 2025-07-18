14b4e183b0a1cccb8ff9dac97e6880018d37d1b7
henricaodopao1 <dragonsxbr@gmail.com>
2025-07-18 13:08:54 +0000

    X00QD: vibrator: Switch to qcom common vibrator-vendor-product.mk
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- device.mk
+++ device.mk
@@ -393,8 +393,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+
 
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
