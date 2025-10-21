1787467b87cafedf6cd006519d62f85fc03c6dfd
Faris <rsuntk@yukiprjkt.my.id>
2025-10-21 12:45:23 +0700

    sdm660-common: biometrics: Rename from 'service-asus_sdm660' to 'service.asus_sdm660'
    Signed-off-by: Faris <rsuntk@yukiprjkt.my.id>

--- sdm660.mk
+++ sdm660.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-asus_sdm660
+    android.hardware.biometrics.fingerprint@2.1-service.asus_sdm660
 
 # FM
 PRODUCT_PACKAGES += \
